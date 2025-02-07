b99dad3f32a330ae361f9e55c7626be1a8c5e28b
MakesMyKart <161360425+MakesMyKart@users.noreply.github.com>
2025-02-07 22:09:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 // script.js
+<script type="module">
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyDQg0KQ2ecP8dRXDN6E0OSW_oZm69xe0bQ",
+        authDomain: "bookmyservicechat.firebaseapp.com",
+        projectId: "bookmyservicechat",
+        storageBucket: "bookmyservicechat.firebasestorage.app",
+        messagingSenderId: "640781387847",
+        appId: "1:640781387847:web:7808c9a8359b4661d28962",
+        measurementId: "G-EYDQSQX12H"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+</script>
+
 function bookService(service, price) {
   var options = {
     "key": "rzp_live_vECzUJjFz7836Q", // Replace with your actual Razorpay API key (you already provided this)
